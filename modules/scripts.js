c6f0139cf98bbe6595a4c3e4c9507940abbc45a6
prajjwalpathak <prajjwalpathak1105@gmail.com>
2025-10-16 22:23:43 +0530

    Add canvas boilerplate script

---
+++ modules/scripts.js
@@ -0,0 +1,21 @@
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 400;
+
+
+
+const init = () => {
+  // Your code
+};
+
+init();
+
+// Animate function
+const animate = () => {
+  requestAnimationFrame(animate);
+  // ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+  // Your code
+};
+
+animate();

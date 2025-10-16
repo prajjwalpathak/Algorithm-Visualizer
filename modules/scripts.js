7efc7f88d5ab79633344c7b955140460649f7d69
prajjwalpathak <prajjwalpathak1105@gmail.com>
2025-10-16 23:02:43 +0530

    Add vertical rectangles

--- modules/scripts.js
+++ modules/scripts.js
@@ -1,21 +1,40 @@
+import { getRandomArray } from "./utils.js";
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 400;
 
+// Rectangle
+
+const drawRectangle = () => {
+    ctx.fillStyle = "rgb(0, 82, 171)";
+    let x = 100;
+    let y = 350;
+    let w = 4;
+    const MIN = 10;
+    const MAX = 250;
+    const FREQ = 100;
+
+    let h = getRandomArray(MIN, MAX, FREQ);
+    for (let idx = 0; idx < FREQ; idx++) {
+        ctx.fillRect(x + idx * 6, y, w, -h[idx]);
+    }
+}
 
 
 const init = () => {
-  // Your code
+    // Your code
+    drawRectangle();
 };
 
 init();
 
 // Animate function
 const animate = () => {
-  requestAnimationFrame(animate);
-  // ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
-  // Your code
+    requestAnimationFrame(animate);
+    // ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+    // Your code
 };
 
 animate();

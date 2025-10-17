52e7ad517c60213c685ebdd413a85d2590fbf92a
prajjwalpathak <prajjwalpathak1105@gmail.com>
2025-10-17 20:53:32 +0530

    Add Selection Sort - Complete, Dynamic bar width

--- modules/scripts.js
+++ modules/scripts.js
@@ -24,21 +24,24 @@ class Bar {
     }
 }
 
-const DELAY = 500;
+const DELAY = 10;
+const FREQ = 50;
+const GAP = 2;
 const X = 100;
 const Y = 350;
-const W = 4;
+const W = (600 - (FREQ * GAP)) / FREQ;
 const MIN = 10;
 const MAX = 250;
-const FREQ = 100;
+
 const BLUE = "rgb(0, 82, 171)";
 const RED = "rgba(171, 0, 0, 1)";
+const GREEN = "rgba(100, 171, 0, 1)";
 let barArray = [];
 
 const setBarArray = () => {
     let h = getRandomArray(MIN, MAX, FREQ);
     for (let idx = 0; idx < FREQ; idx++) {
-        let bar = new Bar(X + idx * 6, Y, W, -h[idx], BLUE);
+        let bar = new Bar(X + idx * (W + GAP), Y, W, -h[idx], BLUE);
         barArray.push(bar);
     }
 }
@@ -53,11 +56,10 @@ const changeBarColor = (idx, color) => {
     barArray[idx].color = color;
 }
 
-const switchBackColor = (idx, color) => {
+async function switchBackColor(idx, color) {
     changeBarColor(idx, color);
-    setTimeout(() => {
-        changeBarColor(idx, BLUE);
-    }, DELAY);
+    await delay(DELAY);
+    changeBarColor(idx, BLUE);
 }
 
 const moveColorBar = (startIdx, endIdx, color) => {
@@ -68,32 +70,10 @@ const moveColorBar = (startIdx, endIdx, color) => {
     }
 }
 
-const swapBar = (firstBarIdx, secondBarIdx) => {
-    setTimeout(() => {
-        [barArray[firstBarIdx].h, barArray[secondBarIdx].h] = [barArray[secondBarIdx].h, barArray[firstBarIdx].h];
-    }, DELAY);
+async function swapBar(firstBarIdx, secondBarIdx) {
+    await delay(DELAY);
+    [barArray[firstBarIdx].h, barArray[secondBarIdx].h] = [barArray[secondBarIdx].h, barArray[firstBarIdx].h];
 }
-
-// const findMin = (startIdx, endIdx) => {
-//     let min = Number.MAX_SAFE_INTEGER;
-//     let minIdx = 0;
-//     for (let idx = startIdx; idx <= endIdx; idx++) {
-//         console.log(idx);
-
-//         setTimeout(() => {
-//             if (-barArray[idx].h < min) {
-//                 changeBarColor(minIdx, BLUE);
-//                 min = -barArray[idx].h;
-//                 minIdx = idx;
-//                 changeBarColor(minIdx, RED);
-//             }
-//             else {
-//                 switchBackColor(idx, RED);
-//             }
-//         }, idx * DELAY);
-//     }
-//     return minIdx;
-// }
 
 const findMinimum = (startIdx, endIdx) => {
     let min = Number.MAX_SAFE_INTEGER;
@@ -107,14 +87,55 @@ const findMinimum = (startIdx, endIdx) => {
     return minIdx;
 }
 
-async function selectionSort() {
-    let minIdx = 0;
-    for (let i = 0; i < FREQ - 1; i++) {
-        await delay(DELAY);
-        // findMin(i,FREQ-1);
-        minIdx = findMinimum(i, FREQ - 1);
-        findMin(i, FREQ - 1);
-        swapBar(i, minIdx);
+// function* selectionSortGenerator(startIdx, endIdx) {
+//     let minIdx = findMinimum(startIdx, endIdx);
+//     yield 1
+//     findMin(startIdx, endIdx);
+//     yield 2
+//     swapBar(startIdx, minIdx);
+//     yield 3
+// }
+
+// const ss = () => {
+//     let i = 0;
+//     let ssInterval = setInterval(() => {
+//         let genObj = selectionSortGenerator(i, FREQ - 1);
+//         genObj.next();
+//         genObj.next();
+//         setTimeout(() => {
+//             genObj.next();
+//         }, (FREQ - i) * DELAY);
+
+//         i++;
+//         if (i == FREQ - 1) {
+//             clearInterval(ssInterval);
+//             console.log("SS complete");
+//         };
+//     }, (FREQ - i) * DELAY);
+// }
+
+
+
+// async function findMin(startIdx, endIdx) {
+//     let min = Number.MAX_SAFE_INTEGER;
+//     let minIdx = 0;
+//     for (let idx = startIdx; idx <= endIdx; idx++) {
+//         await delay(DELAY);
+//         if (-barArray[idx].h < min) {
+//             changeBarColor(minIdx, BLUE);
+//             min = -barArray[idx].h;
+//             minIdx = idx;
+//             changeBarColor(minIdx, RED);
+//         }
+//         else {
+//             switchBackColor(idx, RED);
+//         }
+//     }
+// }
+
+const clearRed = (startIdx, endIdx) => {
+    for (let i = startIdx; i <= endIdx; i++) {
+        changeBarColor(i, BLUE);
     }
 }
 
@@ -122,32 +143,43 @@ const delay = (ms) => {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function findMin(startIdx, endIdx) {
-    let min = Number.MAX_SAFE_INTEGER;
-    let minIdx = 0;
-    for (let idx = startIdx; idx <= endIdx; idx++) {
+async function selectionSort() {
+    const n = FREQ;
 
-        await delay(DELAY);
-        if (-barArray[idx].h < min) {
-            changeBarColor(minIdx, BLUE);
-            min = -barArray[idx].h;
-            minIdx = idx;
-            changeBarColor(minIdx, RED);
+    for (let i = 0; i < n - 1; i++) {
+        clearRed(i, n - 1);
+        let minIdx = i;
+        changeBarColor(i, RED);
+
+        // Step 1: Find smallest element
+        for (let j = i + 1; j < n; j++) {
+            changeBarColor(j, RED);
+            await delay(DELAY);
+
+            if (-barArray[j].h < -barArray[minIdx].h) {
+                changeBarColor(minIdx, BLUE);
+                minIdx = j;
+                changeBarColor(minIdx, RED);
+            } else {
+                changeBarColor(j, BLUE);
+            }
         }
-        else {
-            switchBackColor(idx, RED);
+
+        // Step 2: Swap with first unsorted
+        if (minIdx !== i) {
+            await delay(DELAY);
+            [barArray[i].h, barArray[minIdx].h] = [barArray[minIdx].h, barArray[i].h];
         }
-    }
-}
 
-async function runWithDelay() {
-    for (let i = 1; i <= 5; i++) {
-        console.log(`Iteration ${i}`);
-        await delay(i * 500); // delay 1 second between iterations
+        // Step 3: Mark as sorted
+        changeBarColor(i, GREEN);
+        drawBarArray();
     }
-    console.log("Done!");
-}
 
+    // Mark last bar as sorted
+    changeBarColor(n - 1, GREEN);
+    console.log("Selection Sort Complete");
+}
 
 const init = () => {
     setBarArray();
@@ -158,6 +190,13 @@ const init = () => {
     // findMin(0, FREQ - 1);
     selectionSort();
     // runWithDelay();
+    // const genObj = valueGenerator();
+    // console.log(genObj.next());
+    // console.log(genObj.next());
+    // setTimeout(() => {
+    //     console.log(genObj.next());
+    // }, FREQ * DELAY);
+    // ss();
 };
 
 init();
